b8b8eb6bd445cd93f557ed777cada4a4102e3ba5
driely01 <elyaagoubi.works@gmail.com>
2023-09-24 15:55:45 +0100

    handel some open file errors

--- day01/ex04/main.cpp
+++ day01/ex04/main.cpp
@@ -6,7 +6,7 @@
 /*   By: del-yaag <del-yaag@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/23 16:03:47 by del-yaag          #+#    #+#             */
-/*   Updated: 2023/09/24 14:41:04 by del-yaag         ###   ########.fr       */
+/*   Updated: 2023/09/24 15:54:15 by del-yaag         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,15 @@
 #include <fstream>
 #include "sed.hpp"
 
-int main() {
-	replacingFile( "file", "h", "k ");
+int main( int argc, char **argv ) {
+	
+	( void )argc;
+	if ( argc == 4 ) {
+
+		replacingFile( argv[1], argv[2], argv[3] );
+	} else {
+
+		std::cout << "please enter all three arguments." << std::endl;
+	}
 	return 0;
 }
--- day01/ex04/sed.cpp
+++ day01/ex04/sed.cpp
@@ -6,7 +6,7 @@
 /*   By: del-yaag <del-yaag@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/23 16:04:38 by del-yaag          #+#    #+#             */
-/*   Updated: 2023/09/24 14:38:15 by del-yaag         ###   ########.fr       */
+/*   Updated: 2023/09/24 15:51:43 by del-yaag         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,18 @@ int openFile( std::ifstream& inputFile, std::string filename ) {
 	inputFile.open( filename.c_str(), std::ifstream::in );
 	if ( !inputFile ) {
 
-		std::cout << "error: open: cannot open this file." << std::endl;
+		std::cout << "error: open: cannot open this infile." << std::endl;
+		return 0;
+	}
+	return 1;
+}
+
+int openOutfile( std::ofstream& inputFile, std::string filename ) {
+
+	inputFile.open( filename.c_str(), std::ifstream::in );
+	if ( !inputFile ) {
+
+		std::cout << "error: open: cannot open this outfile." << std::endl;
 		return 0;
 	}
 	return 1;
@@ -80,9 +91,15 @@ int replacingFile( std::string filename, std::string s1, std::string s2 ) {
 	std::string buff;
 	std::string result;
 	std::ifstream inputFile;
-	std::ofstream outputFile( "result" );
+	std::ofstream outputFile;
+	
 	if ( !openFile( inputFile, filename ) )
 		return 0;
+	if ( !openOutfile( outputFile, "result" ) ) {
+		
+		inputFile.close();
+		return 0;
+	}
 	readingFile( inputFile, buff );
 	if ( s1 != s2)
 		findAndReplace( result, buff, s1, s2 );
