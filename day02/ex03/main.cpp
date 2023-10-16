b1bc18d270f4bad0bbd85e130321c4e35029ebfb
driely01 <elyaagoubi.works@gmail.com>
2023-10-16 13:00:24 +0100

    add more tests in bsp ex

--- day02/ex03/main.cpp
+++ day02/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: del-yaag <del-yaag@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 18:23:19 by del-yaag          #+#    #+#             */
-/*   Updated: 2023/10/14 18:46:07 by del-yaag         ###   ########.fr       */
+/*   Updated: 2023/10/16 12:58:26 by del-yaag         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,23 @@
 int main( void ) {
 
 	if ( bsp( Point( 0, 0 ), Point( 10, 0 ), Point( 0, 10 ), Point( 1, 1 ) ) )
-		std::cout << "yes the point is inside the triangle" << std::endl;
+		std::cout << "yes the point is inside the triangle" << std::endl << std::endl;
 	else
-		std::cout << "nah the point isn't inside the triangle" << std::endl;
+		std::cout << "nah the point isn't inside the triangle" << std::endl << std::endl;
+		
+	if ( bsp( Point( 0, 0 ), Point( 10, 0 ), Point( 0, 10 ), Point( -1, 1 ) ) )
+		std::cout << "yes the point is inside the triangle" << std::endl << std::endl;
+	else
+		std::cout << "nah the point isn't inside the triangle" << std::endl << std::endl;
+		
+	if ( bsp( Point( 0, 0 ), Point( 10, 0 ), Point( 0, 10 ), Point( 1, 9 ) ) )
+		std::cout << "yes the point is inside the triangle" << std::endl << std::endl;
+	else
+		std::cout << "nah the point isn't inside the triangle" << std::endl << std::endl;
+		
+	if ( bsp( Point( 0, 0 ), Point( 10, 0 ), Point( 0, 10 ), Point( 5, 5 ) ) )
+		std::cout << "yes the point is inside the triangle" << std::endl << std::endl;
+	else
+		std::cout << "nah the point isn't inside the triangle" << std::endl << std::endl;
 	return 0;
 }
