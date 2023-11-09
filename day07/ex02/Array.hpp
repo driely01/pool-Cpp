cd02ae47887d3c55921e8afa8e213449062bd484
driely01 <elyaagoubi.works@gmail.com>
2023-11-09 18:56:11 +0100

    day07 ex02 almost done

--- day07/ex02/Array.hpp
+++ day07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: del-yaag <del-yaag@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 13:15:18 by del-yaag          #+#    #+#             */
-/*   Updated: 2023/11/09 13:20:25 by del-yaag         ###   ########.fr       */
+/*   Updated: 2023/11/09 18:55:23 by del-yaag         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,34 +14,66 @@
 #define Array_HPP
 
 #include <iostream>
+#include <exception>
 
 template<typename T>
 class Array {
 
 	private:
 		T *array;
+		unsigned int arraySize;
 
 	public:
 		Array( void ) {
 			
+			this->arraySize = 0;
 			this->array = new T[0];
 		}
-		Array( unsigned int n ) {
 
-			this->array = new T[n];
+		Array( unsigned int n ) : arraySize( n ) {
+
+			this->array = new T[arraySize];
+			for ( unsigned int i = 0; i < arraySize; i++ )
+				this->array[i] = T();
 		}
+		
 		Array( const Array &other ) {
 
-			this->array = new T;
+			this->array = new T[this->arraySize];
+			this->arraySize = other.arraySize;
+			
+			for ( unsigned int i = 0; i < arraySize; i++ )
+				this->array[i] = other.array[i];
 			*this = other;
 		}
+		
 		Array &operator=( const Array &rhs ) {
 
 			if ( this == &rhs )
 				return *this;
+				
+			delete[] this->array;
+			this->arraySize = rhs.arraySize;
+			this->array = new T[arraySize];
 			
+			for ( unsigned int i = 0; i < arraySize; i++ )
+				this->array[i] = rhs.array[i];
+			return *this;
 		}
-		~Array( void );
+		
+		T& operator[]( unsigned int index ) {
+
+			if ( index >= this->arraySize ) {
+
+				throw std::out_of_range("this index is out of bounds");
+			}
+			return this->array[index];
+		}
+		
+		unsigned int size( void ) const { return arraySize; }
+		
+		~Array( void ) { delete[] this->array; }
 };
+
 
 #endif
