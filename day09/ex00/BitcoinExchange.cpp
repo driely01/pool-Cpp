f471930254aa7e477bd5a6b4d3443f486d970ef5
driely01 <elyaagoubi.works@gmail.com>
2023-12-13 22:47:22 +0100

    parsing ex00

--- day09/ex00/BitcoinExchange.cpp
+++ day09/ex00/BitcoinExchange.cpp
@@ -6,7 +6,7 @@
 /*   By: del-yaag <del-yaag@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/13 15:49:32 by del-yaag          #+#    #+#             */
-/*   Updated: 2023/12/13 18:31:42 by del-yaag         ###   ########.fr       */
+/*   Updated: 2023/12/13 22:41:54 by del-yaag         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ bool checkDateDigitsAndSlash( std::string &date ) {
 	
 	for ( int i = 0; date[i]; i++ ) {
 
-		if ( !isdigit( date[i] ) && date[i] != '-' && date[i] != ' ' ) {
+		if ( !isdigit( date[i] ) && date[i] != '-' ) {
 			
 			std::cout << "Error: bad input =>" << date << std::endl;
 			return false;
@@ -54,9 +54,9 @@ bool checkYearMonthDay( std::string &date ) {
 	std::string day;
 	std::string orDate;
 
-	double yyyy;
-	double mm;
-	double dd;
+	int yyyy;
+	int mm;
+	int dd;
 
 	orDate = date;
 	year = date.substr( 0, date.find( "-" ) );
@@ -69,14 +69,55 @@ bool checkYearMonthDay( std::string &date ) {
 	std::istringstream( month ) >> mm;
 	std::istringstream( day ) >> dd;
 	
-	if ( yyyy <= 0 || mm > 31 || mm <= 0 || dd > 12 || dd <= 0 ) {
+	if ( yyyy <= 0 || mm > 12 || mm <= 0 || dd > 31 || dd <= 0 ) {
 		
 		std::cout << "Error: bad input => " << orDate << std::endl;
 		return false;
 	}
 
-	std::cout << "|" << yyyy << "-" << mm << "-" << dd << "|" << std::endl;
+	// std::cout << "|" << yyyy << "-" << mm << "-" << dd << "|" << std::endl;
 	
+	return true;
+}
+
+bool parseValue( std::string value ) {
+
+	double bitcoinValue;
+	bool flag = true;
+
+	for ( int i = 0; value[i]; i++ ) {
+
+		if ( !isdigit( value[i] ) ) {
+
+			if ( value[i] == '.' ) {
+				
+				if ( flag == true )
+					flag = false;
+				else {
+					
+					std::cout << "Error: invalid number => " << value << std::endl;
+					return false;
+				}
+			}
+			else if ( ( value[i] == '-' || value[i] == '+' ) && i == 0 )
+				continue;
+			std::cout << "Error: invalid number => " << value << std::endl;
+			return false;
+		}
+	}
+
+	std::istringstream( value ) >> bitcoinValue;
+
+	if ( bitcoinValue < 0 ) {
+		
+		std::cout << "Error:  not a positive number." << std::endl;
+		return false;
+	}
+	else if ( bitcoinValue > 1000 ) {
+
+		std::cout << "Error: too large number." << std::endl;
+		return false;
+	}
 	return true;
 }
 
@@ -87,11 +128,41 @@ bool parseDate( std::string date ) {
 	return false;
 }
 
+bool getFormAndDelimator( std::string buffer, std::string &date, std::string &value, std::string &delimator ) {
+
+	(void)value;
+	(void)delimator;
+	int i = 4;
+	date = buffer.substr( 0, i );
+	if ( date != "date" ) {
+		
+		std::cout << "Error: inalid format." << std::endl;
+		return false;
+	}
+	for ( buffer[i]; i++; ) {
+
+		if ( isalpha( buffer[i] ) )
+			break;
+	}
+	delimator = buffer.substr( 4, i - 4);
+	value = buffer.substr( i, buffer.length() );
+	if ( value != "value" ) {
+		
+		std::cout << "Error: inalid format." << std::endl;
+		return false;
+	}
+	std::cout << date << delimator << value << " " << std::endl;
+	return true;
+}
 
 bool parseInput( char *filename ) {
 
 	std::string buffer;
 	std::ifstream inputFile;
+	std::string dateForm;
+	std::string valueForm;
+	std::string delimator;
+	
 	size_t find;
 
 	inputFile.open( filename, std::ifstream::in );
@@ -102,37 +173,31 @@ bool parseInput( char *filename ) {
 	}
 	
 	getline( inputFile, buffer );
-	if ( buffer != "date | value" ) {
-
-		std::cout << "Error: date | value => not found!" << std::endl;
+	if ( !getFormAndDelimator( buffer, dateForm, valueForm, delimator ) )
 		return false;
-	}
 	
 	while ( !inputFile.eof() ) {
 
-		if ( getline( inputFile, buffer ) ) {
+		if ( getline( inputFile, buffer ) && !buffer.empty()) {
 
 			// find the delimator
 			std::string date = buffer;
 			std::string value;
-			find = buffer.find( "|" );
+			find = buffer.find( delimator );
 			if ( find != std::string::npos ) {
 				
 				date.resize( find );
-				value = buffer.substr(find + 1);
+				value = buffer.substr(find + delimator.length());
 				if ( parseDate( date ) ) {
 					
-					std::cout << date << " => |" << value << "| = " << "chihaja" << std::endl;
+					if ( parseValue( value ) )
+						std::cout << date << " => " << value  << std::endl;
 				}
 			} else {
 
 				std::cout << "Error: bad input => " << buffer << std::endl;
 			}
 		}
-		// else {
-
-		// 	std::cout << buffer << std::endl;
-		// }
 	}
 	inputFile.close();
 	return true;
