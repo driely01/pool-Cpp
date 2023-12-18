55e1861e5d36e91d7eeb87bb8b3adbf21ba9edda
driely01 <elyaagoubi.works@gmail.com>
2023-12-18 18:14:10 +0100

    fixe display issue

--- day09/ex02/PmergeMe.cpp
+++ day09/ex02/PmergeMe.cpp
@@ -6,7 +6,7 @@
 /*   By: del-yaag <del-yaag@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 11:31:55 by del-yaag          #+#    #+#             */
-/*   Updated: 2023/12/18 14:43:00 by del-yaag         ###   ########.fr       */
+/*   Updated: 2023/12/18 18:13:42 by del-yaag         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -106,16 +106,13 @@ void mergeSort( std::vector<std::pair<int, int> > &arr ) {
 // ---------------------START PRINT VECTOR--------------------- //
 void printVector( std::vector<int> &vec, bool before ) {
 
-	// size_t length = vec.size();
 	std::vector<int>::iterator it = vec.begin();
 	if ( before )
 		std::cout << "Before:\t";
 	else
 		std::cout << "After:\t";
 
-	// if ( length > 10 )
-	// 	length = 10;
-	for ( ; it != vec.end(); ++it )
+	for ( size_t i = 0; i < 10 && it != vec.end(); ++it )
 		std::cout << *it << " ";
 	std::cout << "[...]" << std::endl;
 }
